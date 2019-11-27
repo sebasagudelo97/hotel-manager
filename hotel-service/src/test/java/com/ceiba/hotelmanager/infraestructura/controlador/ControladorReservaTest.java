f42d6881b2f96f059452192693d76f851a8cc245
Sebas Agudelo <sagudelo312@gmail.com>
2019-11-27 11:23:49 -0500

    Se crea el test para listar reservas en reserva controlador

--- hotel-service/src/test/java/com/ceiba/hotelmanager/infraestructura/controlador/ControladorReservaTest.java
+++ hotel-service/src/test/java/com/ceiba/hotelmanager/infraestructura/controlador/ControladorReservaTest.java
@@ -2,7 +2,9 @@ package com.ceiba.hotelmanager.infraestructura.controlador;
 
 import com.ceiba.hotelmanager.HotelmanagerApplication;
 import com.ceiba.hotelmanager.aplicacion.comando.ComandoReserva;
+import com.ceiba.hotelmanager.dominio.modelo.Factura;
 import com.ceiba.hotelmanager.dominio.modelo.Habitacion;
+import com.ceiba.hotelmanager.dominio.modelo.Reserva;
 import com.ceiba.hotelmanager.dominio.modelo.Usuario;
 import com.ceiba.hotelmanager.dominio.servicio.habitacion.ServicioCambiarEstadoHabitacion;
 import com.ceiba.hotelmanager.infraestructura.adaptador.repositorio.RepositorioHabitacionImpl;
@@ -23,16 +25,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = HotelmanagerApplication.class)
 @AutoConfigureMockMvc
 @Transactional
 public class ControladorReservaTest {
+    private static final LocalDate FECHA_INGRESO=LocalDate.of(2019,5,15);
+    private static final LocalDate FECHA_SALIDA=LocalDate.of(2019,5,20);
+    private static final LocalDate FECHA_REGISTRO=LocalDate.now();
 
     @Autowired
     private WebApplicationContext wac;
@@ -67,7 +75,35 @@ public class ControladorReservaTest {
         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
 
+    @Test
+    public void listarReservasTest() throws Exception{
+        RepositorioHabitacionImpl repositorioHabitacion = new RepositorioHabitacionImpl(repositorioHabitacionJpa);
+        RepositorioUsuarioImpl repositorioUsuario = new RepositorioUsuarioImpl(repositorioUsuarioJpa);
 
+        Habitacion primeraHabitacion = new Habitacion("201", true, 40000L);
+        repositorioHabitacion.guardar(primeraHabitacion);
+        Habitacion segundaHabitacion = new Habitacion("202", true, 40000L);
+        repositorioHabitacion.guardar(segundaHabitacion);
+
+        Usuario primerUsuario = new Usuario(1L,1036960221L,"Juan", "Sebastian", "Agudelo", "Mejia", "3144568565");
+        repositorioUsuario.guardar(primerUsuario);
+        Usuario segundoUsuario = new Usuario(2L,1036960222L,"Juanes", "Esteban", "Agudelo", "Mejia", "3144668565");
+        repositorioUsuario.guardar(segundoUsuario);
+
+        Factura primeraFactura = new Factura(FECHA_REGISTRO,37200);
+        Factura segundaFactura = new Factura(FECHA_REGISTRO,37200);
+
+        Reserva primeraReserva= new Reserva(1,1036960221L,primerUsuario,"201",2, FECHA_INGRESO,FECHA_SALIDA,primeraFactura);
+        Reserva segumdaReserva= new Reserva(2,1036960222L,primerUsuario,"202",2, FECHA_INGRESO,FECHA_SALIDA,segundaFactura);
+
+        ArrayList<Reserva> reservas = new ArrayList<>();
+        reservas.add(primeraReserva);
+        reservas.add(segumdaReserva);
+
+        mockMvc.perform(get("http://localhost:8080/reserva")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(reservas))).andDo(print()).andExpect(status().isOk());
     }
 }
