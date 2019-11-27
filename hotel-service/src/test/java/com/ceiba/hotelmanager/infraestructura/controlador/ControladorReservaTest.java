72ebb3f02c8d0ee9b20e9e99fe2996358a101c76
Sebas Agudelo <sagudelo312@gmail.com>
2019-11-27 11:45:38 -0500

    Se encuentra un problema al mapear el test de reserva en delete

--- hotel-service/src/test/java/com/ceiba/hotelmanager/infraestructura/controlador/ControladorReservaTest.java
+++ hotel-service/src/test/java/com/ceiba/hotelmanager/infraestructura/controlador/ControladorReservaTest.java
@@ -8,8 +8,12 @@ import com.ceiba.hotelmanager.dominio.modelo.Reserva;
 import com.ceiba.hotelmanager.dominio.modelo.Usuario;
 import com.ceiba.hotelmanager.dominio.servicio.habitacion.ServicioCambiarEstadoHabitacion;
 import com.ceiba.hotelmanager.infraestructura.adaptador.repositorio.RepositorioHabitacionImpl;
+import com.ceiba.hotelmanager.infraestructura.adaptador.repositorio.RepositorioReservaImpl;
 import com.ceiba.hotelmanager.infraestructura.adaptador.repositorio.RepositorioUsuarioImpl;
+import com.ceiba.hotelmanager.infraestructura.convertidor.factura.ConvertidorFactura;
+import com.ceiba.hotelmanager.infraestructura.convertidor.usuario.ConvertidorUsuario;
 import com.ceiba.hotelmanager.infraestructura.repositoriojpa.RepositorioHabitacionJpa;
+import com.ceiba.hotelmanager.infraestructura.repositoriojpa.RepositorioReservaJpa;
 import com.ceiba.hotelmanager.infraestructura.repositoriojpa.RepositorioUsuarioJpa;
 import com.ceiba.hotelmanager.testdatabuilder.aplicacion.ComandoReservaTestDataBuilder;;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,6 +30,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,7 +54,16 @@ public class ControladorReservaTest {
     private ObjectMapper objectMapper;
 
     @Autowired
+    private ConvertidorUsuario convertidorUsuario;
+
+    @Autowired
+    private ConvertidorFactura convertidorFactura;
+
+    @Autowired
     private RepositorioHabitacionJpa repositorioHabitacionJpa;
+
+    @Autowired
+    private RepositorioReservaJpa repositorioReservaJpa;
 
     @Autowired
     private RepositorioUsuarioJpa repositorioUsuarioJpa;
@@ -95,6 +109,7 @@ public class ControladorReservaTest {
         Factura primeraFactura = new Factura(FECHA_REGISTRO,37200);
         Factura segundaFactura = new Factura(FECHA_REGISTRO,37200);
 
+
         Reserva primeraReserva= new Reserva(1,1036960221L,primerUsuario,"201",2, FECHA_INGRESO,FECHA_SALIDA,primeraFactura);
         Reserva segumdaReserva= new Reserva(2,1036960222L,primerUsuario,"202",2, FECHA_INGRESO,FECHA_SALIDA,segundaFactura);
 
@@ -106,4 +121,5 @@ public class ControladorReservaTest {
         .contentType(MediaType.APPLICATION_JSON)
         .content(objectMapper.writeValueAsString(reservas))).andDo(print()).andExpect(status().isOk());
     }
+    
 }
